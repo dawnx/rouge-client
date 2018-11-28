5946b6ff58f9ba4f9377414ed28d80714753324c
zzs <15517139110@163.com>
2018-11-28 16:15:25 +0800

    结构修改~~~~~~~~~~~~

---
+++ rouge_c/src/Util/LayerUtil.ts
@@ -0,0 +1,14 @@
+class LayerUtil {
+	
+	public static shopMain:ShopMain;
+	public static gameMain:GameMain;
+	public static popupMain:egret.Sprite;
+	
+
+	public static get(uri: string,
+		onComplete: Function = null,
+		onError: Function = null,
+		onProgress: Function = null) {
+
+		}
+}

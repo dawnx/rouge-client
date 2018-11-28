9d5383783461fd2f3c9e60334d06c35214acd3d9
phf <infatuationu@163.com>
2018-11-28 18:46:26 +0800

    增加公告弹窗；

--- rouge_c/src/popup/BroadCast.ts
+++ rouge_c/src/popup/BroadCast.ts
@@ -1,21 +1,27 @@
 class BroadCast extends eui.Component {
     private group: eui.Group;
     private _contentText: eui.Label;
-    private _returnBtn: eui.Image;
     private _showIma: eui.Image;
-    private _mainsence: ShopMain;
-    public constructor(mainsence: ShopMain) {
+    private _returnBtn: eui.Image;
+    private _text: string;
+    private _image: string;
+    public constructor(contentText: string, showIma: string) {
         super()
-        this._mainsence = mainsence;
+
+        this._text = contentText;
+        this._image = showIma;
+
         this.skinName = "resource/skin/broadcast.exml";
     }
     public childrenCreated() {     //自执行
         super.childrenCreated();
+
         this.init();
         this._returnBtn.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onclickClose, this);
     }
     private init() {
-
+        this._contentText.text = this._text;
+        this._showIma.source = "resource/assets/game/" + this._image + ".png";
     }
 
     private onclickClose() {
